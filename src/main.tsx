00b567c45bf1d5ab2e9e5cc18144f9a0c6ded55e
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-05-03 11:13:51 +0900

    Update main.tsx
    Router for Betting Result Added

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BettingResult from "./pages/betting-result/index.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/result",
+    element: <BettingResult />,
   },
 ]);
 
