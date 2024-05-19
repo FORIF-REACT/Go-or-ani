433901b99ba14eb3933e45090439802f526a030f
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-05-19 16:45:24 +0900

    Topbar Layout changed
    / Modified
    main.tsx
    root.render 아래 Top 컴포넌트 제거
    
    App.tsx
    Top 컴포넌트 추가 및 기본 레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "./components/ui/button";
 import { Outlet } from "react-router-dom";
+import Top from "./top";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="h-screen w-screen flex flex-col gap-4 justify-center items-center">
-      <h1 className="text-3xl">React-TailwindCSS-Typescript-Vite-shadcn/ui</h1>
-      <h1 className="text-2xl">Repo for Go-or-Ani Project</h1>
-      <h1 className="text-5xl font-bold">{count}</h1>
-      <div className="flex flex-row gap-5">
-        <Button size={"icon"} onClick={() => setCount((count) => count + 1)}>
-          +
-        </Button>
-        <Button size={"icon"} onClick={() => setCount((count) => count - 1)}>
-          -
-        </Button>
+    <div className="w-screen gap-4">
+      <div className="fixed top-0 w-full">
+        <Top />
       </div>
-      <Outlet/>
+
+      <div className="w-full flex flex-col items-center mt-20">
+        <Outlet />
+      </div>
     </div>
   );
 }
-
 
 export default App;
