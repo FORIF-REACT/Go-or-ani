de8efb5cc552607e0100328c296303be8e8528c3
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-05-19 22:27:14 +0900

    배경 색 수정, 결과 페이지 수정
    배경 색 수정, 결과 페이지 수정
    
    index.css
    --background (배경색 #151515로 수정)
    --foreground (기본색 #FFFFFF로 수정)
    
    결과 페이지 디자인에 맞게 수정

--- src/pages/betting-result/Percentagebar.tsx
+++ src/pages/betting-result/Percentagebar.tsx
@@ -1,34 +1,38 @@
-/**
- * Percentage bar Component
- * * Wrote by SJ
- * @param percent This parameter is for percentage of one of choices e.g. 0.3 for (30%)
- * @returns Component that shows percentage of each choices
- *
- * TODO None
- */
-
-export default function Percentagebar({ percent }: { percent: number }) {
+export default function PercentageBar({
+  title,
+  ratio,
+  selections,
+}: {
+  title: string;
+  ratio: number;
+  selections: string[];
+}) {
   return (
-    <>
-      <div className="w-full flex flex-row justify-between">
-        <div className="text-slate-400">
-          {Math.round(percent * 10000) / 100}
-        </div>
-        <div className="text-slate-600">
-          {Math.round((1 - percent) * 10000) / 100}
-        </div>
-      </div>
+    <div className="flex flex-col gap-2">
+      <div>{title}</div>
+
       <div className="w-full flex flex-row">
         <div
-          style={{ flexBasis: `${percent * 100}%` }}
-          className={`h-6 bg-slate-400 rounded-l-md hover:shadow-[0_0_20px_-5px_rgba(0,0,0,0.1)] hover:shadow-slate-400 transition-all`}
+          style={{ flexBasis: `${ratio * 100}%` }}
+          className={`h-10 bg-primary-green-300 rounded-l-md animate-in`}
         />
-        <div
-          className={
-            "h-6 flex-auto bg-slate-600 rounded-r-md hover:shadow-[0_0_20px_-5px_rgba(0,0,0,0.1)] hover:shadow-slate-600 transition-all"
-          }
-        />
+        <div className={"h-10 flex-auto bg-primary-green-600 rounded-r-md"} />
       </div>
-    </>
+
+      <div className="w-full flex flex-row justify-between">
+        <div className="flex flex-col justify-start">
+          <div className="font-bold opacity-50">
+            {Math.round(ratio * 10000) / 100}
+          </div>
+          <div className="opacity-50">{selections[0]}</div>
+        </div>
+        <div className="flex flex-col justify-start">
+          <div className="font-bold opacity-50">
+            {Math.round((1 - ratio) * 10000) / 100}
+          </div>
+          <div className="opacity-50">{selections[1]}</div>
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ src/pages/betting-result/User.tsx
@@ -0,0 +1,8 @@
+export default function User({ id }: { id: string }) {
+  return (
+    <div className="flex flex-row">
+      <div className="text-2xl pr-2">{"😁"}</div>
+      <div className="font-bold text-2xl"> {id}</div>
+    </div>
+  );
+}
