58d275e9ffb5ff72ef63863e974dbc78825e7889
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-05-20 12:37:23 +0900

    관리자 페이지 수정
    변화된 디자인에 맞게 관리자 페이지 수정

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,8 +9,10 @@ type Menu = {
 
 const menus: Menu[]= [
   {title: "대시보드", link: ""},
+  {title: "divider", link:""},
   {title: "사용자 추가", link: "adduser"},
   {title: "사용자 정보 수정 / 삭제", link: "modifyuser"},
+  {title: "divider", link:""},
   {title: "베팅 정보 수정 / 삭제", link: "modifybet"},
 ];
 
@@ -18,25 +20,14 @@ export default function Admin() {
   const [selected, setSelected] = useState<number>(0);
 
   return (
-    <div className="w-[768px] flex flex-row">
+    <div className="flex flex-row w-[1024px] mt-2">
       <div className="flex-shrink-0">
         <Sidebar menus={menus} selected={selected} setSelected={setSelected} />
       </div>
 
-      <div className="w-full ml-6">
+      <div className="w-full ml-4">
         <Outlet/>
       </div>
     </div>
   );
 }
-
-// function Controller({ menu }: { menu: string }) {
-//   switch (menu) {
-//     case "사용자 추가":
-//       return <UserAdd />;
-//     case "사용자 정보 수정 / 삭제":
-//       return <UserModify />;
-//     case "베팅 정보 수정":
-//       return <BetModify />;
-//   }
-// }
