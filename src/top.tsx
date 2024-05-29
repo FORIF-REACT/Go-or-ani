865ad30c56e3688e4e87d19d4c75de713ea01e6b
Your Name <you@your-domain.com>
2024-05-29 12:57:19 +0900

    HG top initial design and gradient

--- src/top.tsx
+++ src/top.tsx
@@ -1,33 +1,101 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel
+} from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import './index.css';
+
+const navigation = [
+  { name: '진헹중인 베팅', href: '/bettinglist', current: false },
+  { name: '종료된 베팅', href: '/result', current: false },
+  { name: '나의 베팅', href: '/create', current: false },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 function Top() {
-
-  const userId = '123'; //임시부여한 아이디
+  const userId = '123'; // 임시부여한 아이디
 
   return (
-    <div>
-      <nav className="flex items-center justify-between flex-wrap bg-background-black-950 p-6">
-        <div className="App">
-          <div className="flex items-center flex-shrink-0 text-primary-green-300 mr-6">
-            <span className="font-semibold text-xl tracking-tight">
-              Go-or-Ani
-            </span>
-            <Link to="/">Go-or-Ani</Link>
+    <Disclosure as="nav" className="bg-background-black-950 relative">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-primary-green-300 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link to="/" className="font-['Music-Vibes'] text-2xl tracking-tight text-primary-green-300">
+                    Go-or-Ani
+                  </Link>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className={classNames(
+                            item.name === '나의 베팅'
+                            ? 'text-primary-purple-600 hover:bg-gray-700 hover:text-white'
+                            : 'text-primary-green-300 hover:bg-gray-700 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link to={`/profile/${userId}`} className="text-primary-green-300 hover:text-green-500">
+                  Profile
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="NowBetting">
-          <Link to="/betting">NowBetting</Link> @수정필요
-        </div>
-        <div className="BettingResult">
-          <Link to="result">BettingResult</Link> @수정필요
-        </div>
-        <div className="ProfileLink">
-          <Link to={`/profile/${userId}`} className="text-primary-green-300 hover:text-green-500">
-            Profile
-          </Link>
-        </div>
-      </nav>
-    </div>
+
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as={Link}
+                  to={item.href}
+                  className={classNames(
+                    item.name === '나의 베팅'
+                    ? 'text-primary-purple-600 hover:bg-gray-700 hover:text-white'
+                    : 'text-primary-green-300 hover:bg-gray-700 hover:text-white',
+                    'rounded-md px-3 py-2 text-sm font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </DisclosurePanel>
+          <div className="absolute bottom-0 left-0 right-0 h-0.5 gradient-line" />
+        </>
+      )}
+    </Disclosure>
   );
 }
 
